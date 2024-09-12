1419b18a0d67d40e3d8adfeb7cb924f6bf24f4a4
Naresh Narayan <88131733+nareshnarayanofficial@users.noreply.github.com>
2024-09-13 03:45:11 +0530

    Update portify.sh

--- portify.sh
+++ portify.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+#Mentioning color codes for the tool
+
+#Mentioning color codes for the tool
+
+PORT-SCANNING-TOOL='\033[0;31m'
+NARESHNARAYAN='\033[0m'
+
+echo " __     ___    ___    _______   _   ____  __    _
+echo "|   \  / _ \  |  _ \ |__   __| | | |  __| \ \  / /
+echo "| | | | / \ | | |_| |   | |    | | | |__   \ \/ /
+echo "|  _/ | | | | |    /    | |    | | |  __|   |  |
+echo "| |   | \_/ | | |\ \    | |    | | | |      |  |
+echo "|_|    \___/  |_| \_\   |_|    |_| |_|      |__|
+echo ""
+echo -e "${PORT-SCANNING-TOOL}DEVELOPED BY${NARESHNARAYAN}"
+
+
 
