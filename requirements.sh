33a25370eaf77ab547a73b5c72bd735d8e92518d
Naresh Narayan <88131733+nareshnarayanofficial@users.noreply.github.com>
2024-09-13 04:09:52 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,1 +1,20 @@
+#!/bin/bash
+
+
+echo "CHOOSE AN OPTION:"
+echo "1. INSTALL NMAP"
+echo "2. UPDATE NMAP"
+
+case $choice in
+1) if command -v nmap &> /dev/null
+then
+    echo "nmap IS INSTALLED."
+else
+    sudo apt install nmap
+fi
+2) if command -v nmap &> /dev/null
+then
+sudo apt update && upgrade nmap
+fi
+
 
