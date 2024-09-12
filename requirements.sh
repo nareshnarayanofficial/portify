9cc6422a78a903d4ea0a538e8b4a35b2b32fba4d
Naresh Narayan <nareshnarayanofficial@gmail.com>
2024-09-13 05:16:03 +0530

    Update requirements.sh

--- requirements.sh
+++ requirements.sh
@@ -1,20 +1,48 @@
 #!/bin/bash
 
+#Mentioning color codes for the tool
+RED='\033[0;31m'
+NEON='\033[0;92m'
+NEONY='\033[1;93m'
+Y='\033[0;33m'
+NC='\033[0m' # No Color
 
+
+echo -e "${Y}"
+echo -e "     __     ___    ___    _______   _   ____  __    __"
+echo -e "    |   \  / _ \  |  _ \ |__   __| | | |  __| \ \  / /"
+echo -e "    | | | | / \ | | |_| |   | |    | | | |__   \ \/ / "
+echo -e "    |  _/ | | | | |    /    | |    | | |  __|   |  |  "
+echo -e "    | |   | \_/ | | |\ \    | |    | | | |      |  |  "
+echo -e "    |_|    \___/  |_| \_\   |_|    |_| |_|      |__| ${NC} "
+echo -e ""
+echo -e "${RED}PORT-SCANNING-TOOL DEVELOPED BY$ NARESH NARAYAN${NC}"
+echo ""
+echo -e "${Y}DISCLAIMER: This script is provided for project work purposes only. It is not intended for production use or any commercial applications.${NC}"
+echo ""
+
+echo ""
 echo "CHOOSE AN OPTION:"
-echo "1. INSTALL NMAP"
-echo "2. UPDATE NMAP"
+echo -e "${RED}1. INSTALL NMAP"
+echo -e "1. INSTALL NETCAT [Not Working]"
+echo -e "2. UPDATE NMAP [Not Working]${NC}"
+echo ""
 
-case $choice in
-1) if command -v nmap &> /dev/null
-then
-    echo "nmap IS INSTALLED."
-else
-    sudo apt install nmap
-fi
-2) if command -v nmap &> /dev/null
-then
-sudo apt update && upgrade nmap
-fi
+echo -e "${YELLOW}UPDATING PACKAGES...${NC}"
+sudo apt-get update
+
+#INSTALLING NMAP
+echo -e "${YELLOW}INSTALLING nmap${NC}"
+sudo apt-get install -y nmap
+echo ""
+echo ""
+echo -e "${RED}nmap INSTALLED SUCCESSFULLY!${NC}"
+
+#MAKING PORTIFY.SH FILE EXEC...
+chmod +x portify.sh
+
+
+
+
 
 
